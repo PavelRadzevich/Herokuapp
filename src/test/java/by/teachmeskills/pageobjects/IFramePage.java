420052a3babdb47bf0dc7c171736f9c2d08039a4
PavelRadzevich <pavel.radzevich@gmail.com>
2023-02-20 05:03:43 +0300

    IHA-002_selenium_advanced. IFramesTest added. IFramePage edited.

--- src/test/java/by/teachmeskills/pageobjects/IFramePage.java
+++ src/test/java/by/teachmeskills/pageobjects/IFramePage.java
@@ -1,5 +1,6 @@
 package by.teachmeskills.pageobjects;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -19,6 +20,11 @@ public class IFramePage extends BasePage {
     @FindBy(xpath = "//body[@id='tinymce']/p")
     private WebElement textBox;
 
+    @FindBy(xpath = "//p[@style='text-align: center;']")
+    private WebElement textAlign;
+    @FindBy(xpath = "//button[@title='Align center']")
+    private WebElement btnAlignCenter;
+
     public IFramePage() {
         PageFactory.initElements(driver, this);
     }
@@ -28,5 +34,22 @@ public class IFramePage extends BasePage {
         wait.until(ExpectedConditions.visibilityOf(menuForm));
         driver.switchTo().frame(iframeBox);
         return textBox.getText().equals(msg);
+    }
+
+    public IFramePage editTextFrame(String msg) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        wait.until(ExpectedConditions.visibilityOf(menuForm));
+        driver.switchTo().frame(iframeBox);
+        textBox.sendKeys(Keys.chord(Keys.CONTROL, "a"));
+        textBox.sendKeys(msg);
+        return this;
+    }
+
+    public boolean alignCenter() {
+        driver.switchTo().defaultContent();
+        btnAlignCenter.click();
+        driver.switchTo().frame(iframeBox);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        return wait.until(ExpectedConditions.visibilityOf(textAlign)).isDisplayed();
     }
 }
