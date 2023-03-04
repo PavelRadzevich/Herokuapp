61d1f9f04086f9e68ad1bce4120c32e6e99816c7
Pavel <pavel.radzevich@gmail.com>
2023-03-04 19:57:12 +0300

    IHA-003_demoqa_com. Edited BaseTest.

--- src/test/java/by/teachmeskills/tests/BaseTest.java
+++ src/test/java/by/teachmeskills/tests/BaseTest.java
@@ -13,11 +13,12 @@ import java.util.HashMap;
 
 abstract public class BaseTest {
     protected WebDriver driver;
+    protected static final int TIMOUT = 15;
 
     @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
-        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+        HashMap<String, Object> chromePrefs = new HashMap<>();
         chromePrefs.put("profile.default_content_settings.popups", 0);
         chromePrefs.put("safebrowsing.enabled", "false");
         chromePrefs.put("download.default_directory", "user.home" + "/Downloads");
@@ -25,14 +26,15 @@ abstract public class BaseTest {
         options.setExperimentalOption("prefs", chromePrefs);
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(TIMOUT));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TIMOUT));
         BasePage.setDriver(driver);
     }
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
         if (driver != null) {
+            driver.close();
             driver.quit();
         }
     }
